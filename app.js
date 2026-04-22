eed62874fea226fe15f65e4ea90892a6a2971a59
mtuoreniemi <mtuoreniemi@gmail.com>
2026-04-22 14:07:42 +0300

    päivitetty

--- app.js
+++ app.js
@@ -1,78 +1,78 @@
 // Kesäviikkojen tiedot + linkit viikkosivuille
-const weekData = {
-  1: { 
+let weekData = {
+  21: { 
     title: "Retki Mustikkamaalle", 
     text: ".", 
     link: "viikko1.html" 
   },
 
-  2: { 
+  22: { 
     title: "YLEX Pop festivaali", 
     text: "tekstiä.", 
     link: "viikko2.html" 
   },
 
-  3: { 
+  23: { 
     title: "Nuuksion retki", 
     text: "Luontopolku + eväät.", 
     link: "viikko3.html" 
   },
 
-  4: { 
+  24: { 
     title: "Helsinki Samba Carneval", 
     text: "-", 
     link: "viikko4.html" 
   },
 
-  5: { 
+  25: { 
     title: "Jalkapallo MM-kisat", 
     text: "Kisa katsomo kotona.", 
     link: "viikko5.html" 
   },
 
-  6: { 
+  26: { 
     title: "Helsinki PRIDE", 
     text: "Pride kulkue.", 
     link: "viikko6.html" 
   },
 
-  7: { 
+  27: { 
     title: "Seinäkiipeilyä", 
     text: "-", 
     link: "viikko7.html" 
   },
 
-  8: { 
+  28: { 
     title: "Korkeasaaren eläintarha", 
     text: "-", 
     link: "viikko8.html" 
   },
 
-  9: { 
+  29: { 
     title: "Dallapén pihakirppis", 
     text: "Aleksis Kiven kadulla.", 
     link: "viikko9.html" 
   },
 
-  10: { 
+  30: { 
     title: "Laguuni Keilis", 
     text: "Vesipuisto aktivitteettejä Keilaniemessä.", 
     link: "viikko10.html" 
   },
 
-  11: { 
+  31: { 
     title: "Ranta päivä", 
     text: "-", 
     link: "viikko11.html" 
   },
 
-  12: { 
+  32: { 
     title: "Kumpulan maauimailman", 
     text: "-", 
     link: "viikko12.html" 
   },
 
-  13: { 
+  33: { 
     title: "Linnanmäki", 
     text: "-", 
     link: "viikko13.html" 
@@ -80,12 +80,12 @@ const weekData = {
 };
 
 // Mikä viikko on menossa (voit muuttaa)
-const currentWeek = 3;
+let currentWeek = 23; // tämä pitää muokata aktiiviseksi kun kalenteri palautetaan
 
 // Luo 13 luukkua automaattisesti
 const calendar = document.getElementById("calendar");
 
-for (let i = 1; i <= 13; i++) {
+for (let i = 21; i <= 33; i++) {
   const door = document.createElement("div");
   door.classList.add("door");
   door.dataset.week = i;
